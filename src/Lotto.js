3eac55596af69e4db1927831430b93f8686eae81
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-09-18 16:06:58 +0900

    로또 테스트 케이스 작성 및 로또 클래스 완성

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,17 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== 6) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
+
+    for (const num of numbers) {
+      if (!Number.isInteger(num) || num < 1 || num > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    }
   }
 
   // TODO: 추가 기능 구현
